bbb3546b9575e84cb5c02c86e27a5077f6fb6ca1
zhangchuanyangwd <72744426+zhangchuanyangwd@users.noreply.github.com>
2020-10-23 15:08:27 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		CPU cpu = new CPU();
 		cpu.setSpeed(2200);
+		cpu.setBrand(Inter);
 		HardDisk disk = new HardDisk();
 		disk.setAmount(200);
 		PC pc = new PC();
