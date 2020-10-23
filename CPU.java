d9f4f2c34b9773e2a0455f93587da49f8b4ac067
zhangchuanyangwd <72744426+zhangchuanyangwd@users.noreply.github.com>
2020-10-23 15:07:15 +0800

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -2,12 +2,18 @@ package com;
 
 public class CPU {
 	private int speed;
-	
+	private String brand;
 	public int getSpeed() {
 		return speed;
 	}
+	public Sting getBrand() {
+		return brand;
+	}
 	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
 	}
 
 }
